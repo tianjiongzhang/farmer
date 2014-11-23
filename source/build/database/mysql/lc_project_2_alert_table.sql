70c60d16d373ec51e799ff7ebd88ec16d1784c6e
farmer-dev1 <farmer-dev1@bitbucket.org>
2014-11-23 14:11:54 +0800

    增加修改项目定义表

---
+++ source/build/database/mysql/lc_project_2_alert_table.sql
@@ -0,0 +1,12 @@
+﻿#########################################################################
+## 基金会模块
+#########################################################################
+
+#
+# 项目定义表： LC_PROJECT
+#
+ALTER TABLE LC_PROJECT MODIFY AMOUNT DOUBLE(10,2)  COMMENT '项目投入金额' ;
+ALTER TABLE LC_PROJECT MODIFY START_TIME DATETIME  COMMENT '计划开始时间' ;
+ALTER TABLE LC_PROJECT MODIFY START_TIME DATETIME  COMMENT '计划开始时间' ;
+ALTER TABLE LC_PROJECT MODIFY VOLUNTEER_FEE DOUBLE(2,2)  COMMENT '志愿者费用比率' ;
+ALTER TABLE LC_PROJECT MODIFY COOPERATION_FEE DOUBLE(2,2)  COMMENT '志愿者费用比率'; 
